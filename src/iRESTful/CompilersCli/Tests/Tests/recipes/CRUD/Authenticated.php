fce6f38dfb4713122e39b47874c72a110b02d7d0
Steve Rodrigue <steve@irestful.com>
2017-01-31 14:02:38 -0500

    Changed the cli name and updated the recipe for tests.

--- src/iRESTful/CompilersCli/Tests/Tests/recipes/CRUD/Authenticated.php
+++ src/iRESTful/CompilersCli/Tests/Tests/recipes/CRUD/Authenticated.php
@@ -1,12 +1,39 @@
 <?php
 
-function authenticated_crud_validateBaseUrl(string $test) {
+function constructor_comboTest_validateParameters($endpoint = null, $api = null, $registration = null) {
+
+    $amount = (empty($endpoint) ? 0 : 1) + (empty($api) ? 0 : 1) + (empty($registration) ? 0 : 1);
+    if ($amount != 1) {
+        throw new \Exception('There must be either an endpoint, an api or a registration.  '.$amount.' given.');
+    }
+
+};
+
+function constructor_pattern_validateParameters($regexPattern = null, $specificUri = null) {
+
+    if ((!empty($regexPattern) && !empty($specificUri)) || (empty($regexPattern) && empty($specificUri))) {
+        $regexPattern = '';
+        $specificUri = null;
+    }
+
+};
+
+function constructor_paramPattern_validateParameters($regexPattern = null, $specificValue = null) {
+
+    if ((!empty($regexPattern) && !empty($specificValue)) || (empty($regexPattern) && empty($specificValue))) {
+        $regexPattern = '';
+        $specificValue = null;
+    }
+
+};
+
+function validateBaseUrl(string $test) {
     if (filter_var($test, FILTER_VALIDATE_URL) === false) {
         throw new \Exception('The baseUrl ('.$test.') is invalid.');
     }
 };
 
-function authenticated_crud_validateUri(string $uri) {
+function validateUri(string $uri) {
 
     if (strpos($uri, '/') !== 0) {
         throw new \Exception('The first character of the URI ('.$uri.') must be a forward slash (/).');
@@ -18,7 +45,7 @@ function authenticated_crud_validateUri(string $uri) {
 
 };
 
-function authenticated_crud_validateHashedPassword(string $hashedPassword) {
+function validateHashedPassword(string $hashedPassword) {
 
     $info = password_get_info($hashedPassword);
     if (!isset($info['algo']) || ($info['algo'] != \PASSWORD_DEFAULT)) {
@@ -27,7 +54,7 @@ function authenticated_crud_validateHashedPassword(string $hashedPassword) {
 
 };
 
-function authenticated_crud_validateKeyname(string $keyname) {
+function validateKeyname(string $keyname) {
 
     if (empty($keyname)) {
         throw new \Exception('The keyname ('.$keyname.') must be a non-empty string.');
